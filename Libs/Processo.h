047e35a3bf833a5e8451919f79346acf1adda239
joaoVGvieira <joaovitorgv2019@gmail.com>
2021-12-10 11:16:01 -0300

    libs

---
+++ Libs/Processo.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int PID;
+    //depois olha tempo
+    int Prioridade;
+} Processo; 
+
+//set(definir valor na variável) PID
+//set(definir valor na variável) tempo
+//set(definir valor na variável) prioridade
+
+//get(pegar valor da variável) PID
+//get(pegar valor da variável) tempo
