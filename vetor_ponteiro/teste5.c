0d1f607de2ba5118710dc3671d68fceaf854136e
Iury Martins Pereira <iurymartins46@gmail.com>
2021-12-11 16:07:54 -0300

    Referência para vetores e ponteiros

--- vetor_ponteiro/teste5.c
+++ vetor_ponteiro/teste5.c
@@ -1,35 +1,32 @@
-// Tem que fazer funcionar, pelo geito que eu imaginei Iury
 #include <stdio.h>
 #include <stdlib.h>
 // Criação/manipulação de um vetor dinamicamente alocado.
-void aloca(TRegistro **p, int n){
-  //TRegistro *p;
-  p = (TRegistro*)malloc(n * sizeof(TRegistro));
-  }
-void inprime(int num){
-    TRegistro *p;
-    int i;
-    for(i = 0; i < num; i++){
-      printf("Idade %d  salario %.2lf\n");
-    }
-}
-void preeenche(int num){
-    TRegistro *p;
-    int i;
-    for(i = 0; i < num; i++){
-      p[i].idade = i;
-      p[i].salario = 2 * 1;
-    }
-}
 typedef struct{
   int idade;
   double salario;
   }TRegistro;
 
+void aloca(TRegistro **p, int n){
+  (*p) = (TRegistro*)malloc(n * sizeof(TRegistro));
+  }
+void inprime(TRegistro **p, int num){
+    int i;
+    for(i = 0; i < num; i++){
+      printf("Idade %d    salario %.2lf\n", (*p)[i].idade, (*p)[i].salario);
+    }
+}
+void preeenche(TRegistro **p,int num){
+    int i;
+    for(i = 0; i < num; i++){
+      (*p)[i].idade = i;
+      (*p)[i].salario = 2 * i;
+    }
+} 
 int main(){
   TRegistro *p;
   aloca(&p,10);
-  inprime(10);
-  preeenche(10);
+  preeenche(&p, 10);
+  inprime(&p,10);
   return 0;
+  system("pause");
   }
