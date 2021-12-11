990fb8d1e62fef3f370a9323f3fd6fc73f58c4ce
Iury Martins Pereira <iurymartins46@gmail.com>
2021-12-11 11:56:08 -0300

    lista_processo

--- Libs/lista_Processos.h
+++ Libs/lista_Processos.h
@@ -4,12 +4,10 @@
 
 typedef struct{
     Processos pid;
-    int celula_anterior;
-    int celula_proxima;
 }Celulas;
-typedef struct lista *lista_Processos;
-typedef struct lista {
-    Celulas celulas;
-};
 
-int* set_criar_lista_processo(lista_Processos lista_processo ,int numero_celulas);
+typedef struct{
+    Celulas *celulas;
+}lista_Processos;
+
+void cria_alocacao(Celulas **celulas, int numero_celula);
--- Sources/lista_Processos.c
+++ Sources/lista_Processos.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* set_criar_lista_processo(lista_Processos lista_processo, int numero_celulas){
-    lista_processo = (lista_Processos*) malloc(numero_celulas * sizeof(lista_Processos));
-    return lista_processo;
+void cria_alocacao(Celulas **celulas, int numero_celula){
+    *celulas = (Celulas*) malloc(numero_celula * sizeof(Celulas));
 }
