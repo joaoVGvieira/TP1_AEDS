349b7802950eeda16c39971a32f0315fce0ba9a2
joaoVGvieira <joaovitorgv2019@gmail.com>
2021-12-14 10:44:22 -0300

    ler arquivo

--- Sources/arquivos.c
+++ Sources/arquivos.c
@@ -10,5 +10,26 @@ void ler(char nome[]){
         exit(1);
     }
     printf("\n\nAberto com sucesso!!!!\n\n");
+
+    int N;
+    // Tamanho do vetor para cursores
+    int NLO;
+    //Número de linhas de operações especificadas abaixo
+    int Op, Qt;
+    // Op = 0 para inserção e 1 para remoção início
+    // Qt = quantidade de vezes que a operação será realizada
+    
+    if (file)
+    {
+        fscanf(file,"%d\n",&N);
+        fscanf(file,"%d\n",&NLO);
+    }
+    for (int i = 0; i < NLO; i++)
+    {
+        fscanf(file,"%d %d",&Op,&Qt);
+        printf("OP=%d\n QT=%d\n",Op, Qt);
+    }
+    
+    fclose(file);
 }
 
